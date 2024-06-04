6975ec5c0ed0fcd0b54d3f818a29ba8aaafcea9b
A_Ariannejad <ariannejad14@gmail.com>
2024-06-05 01:58:33 +0330

    Added: CustomNews model

--- NewsCrawler/CustomNews/models.py
+++ NewsCrawler/CustomNews/models.py
@@ -8,4 +8,9 @@ class CustomNew(models.Model):
     pubDate_ad = models.DateTimeField()
     pubDate_solar = models.DateTimeField()
     description = models.TextField(max_length=250, null=True, blank=True)
+    STATUS_TYPE = (
+        ('latest', 'service'),
+        ('most_visited', 'person_to_site'),
+    )
+    status = models.CharField(max_length=20, choices=STATUS_TYPE, default='latest')
     create_date = models.DateTimeField(auto_now_add=True)
