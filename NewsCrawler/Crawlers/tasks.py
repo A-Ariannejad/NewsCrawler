dd98ca8ce3328cca2d02edc17b5c9af828815c91
A_Ariannejad <ariannejad14@gmail.com>
2024-06-04 17:02:39 +0330

    Added: Crawler Script ready to be merged with 5 min repetition

--- NewsCrawler/Crawlers/tasks.py
+++ NewsCrawler/Crawlers/tasks.py
@@ -1,72 +1,38 @@
 import threading
 import time
-import feedparser
+import requests
 import json
 import requests
-from bs4 import BeautifulSoup
+import xml.etree.ElementTree as ET
 
-def save_feed_dict(feed_dict , category):
-    with open(f"{category}.json" , "w" , encoding="utf-8") as f:
-        json.dump(feed_dict , f , indent=4)
+def parse_xml_response(xml_content):
+    items = {}
+    root = ET.fromstring(xml_content)
+    for item in root.findall('.//item'):
+        x = {}
+        x['title'] = item.find('title').text
+        x['pubDate'] = item.find('pubDate').text
+        x['link'] = item.find('link').text
+        x['id'] = str(x['link']).split("/")[-2],
+        description_element = item.find('description')
+        x['description'] = description_element.text if description_element is not None else ""
+        items[x['id']] = x
+    return items
 
-def get_news_text(url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        with open("temp.html" , "wb") as f:
-            f.write(response.content)
-        soup = BeautifulSoup(response.content, "html.parser")
-        text_div = soup.find("div" , class_="row baznashr-body")
-        if text_div:
-            paragraphs = text_div.find_all("p")
-            raw_text = ""
-            for paragraph in paragraphs:
-                raw_text += paragraph.text + "\n"
-            return raw_text
-        return "-"
-    else:
-        print(f"Failed to get Raw Text of news: {url} , status_code: {response.status_code}")
-        return False
-    
-def parse_feed(rss_url , category):
-    feed = feedparser.parse(rss_url)    
-    if feed.status == 200:
-            results = []
-            for entry in feed.entries:
-                raw_text = get_news_text(entry.link)
-                if raw_text == False:
-                    continue                    
-                results.append({
-
-                    "title": entry.title,
-                    "category": category,
-                    "pubDate": entry.published_parsed,
-                    "link": entry.link,
-                    "id": str(entry.link).split("/")[-2],
-                    "text": raw_text
-                })
-            return results
-    else:
-        print("Failed to get RSS feed. Status code:", feed.status)
-        return False
-        
-def start_crawing():
-    feeds = [
-        {"category": "all_news_mostvisited" , "url": "https://www.yjc.ir/fa/rss/all/mostvisited"},
-        {"category":"all_news_latest" , "url":"https://www.yjc.ir/fa/rss/allnews"}
-         ]
-    
-    for feed in feeds:
-        try:
-            feed_dict = parse_feed(feed["url"] , feed["category"])
-            if feed_dict != False:
-                save_feed_dict(feed_dict , feed["category"])
-            else:
-                print(f"error in getting feed:{feed} reason: Connection Error")
-                
-            print(f"got feed:{feed} successfully")
-            
-        except Exception as e:
-            print(f"error in getting feed:{feed} with error of {e}")
+def save_response_to_file(url, filename):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            with open(filename, 'wb') as file:
+                file.write(response.content)
+            print("Response saved successfully to", filename)
+            return response.content
+        else:
+            print("Error:", response.status_code)
+            return None
+    except requests.exceptions.RequestException as e:
+        print("Error:", e)
+        return None
 
 def my_periodic_task():
     print("Running periodic task...")
