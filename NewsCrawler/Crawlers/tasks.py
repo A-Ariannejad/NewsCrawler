53af12c790918cb2a633d35d21427ff829794575
A_Ariannejad <ariannejad14@gmail.com>
2024-06-05 00:29:01 +0330

    Fixed: thread system to not run twice every x second

--- NewsCrawler/Crawlers/tasks.py
+++ NewsCrawler/Crawlers/tasks.py
@@ -1,53 +1,31 @@
 import threading
+import atexit
 import time
-import requests
-import json
-import requests
-import xml.etree.ElementTree as ET
+import os
 
-def parse_xml_response(xml_content):
-    items = {}
-    root = ET.fromstring(xml_content)
-    for item in root.findall('.//item'):
-        x = {}
-        x['title'] = item.find('title').text
-        x['pubDate'] = item.find('pubDate').text
-        x['link'] = item.find('link').text
-        x['id'] = str(x['link']).split("/")[-2],
-        description_element = item.find('description')
-        x['description'] = description_element.text if description_element is not None else ""
-        items[x['id']] = x
-    return items
-
-def save_response_to_file(url, filename):
-    try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            with open(filename, 'wb') as file:
-                file.write(response.content)
-            print("Response saved successfully to", filename)
-            return response.content
-        else:
-            print("Error:", response.status_code)
-            return None
-    except requests.exceptions.RequestException as e:
-        print("Error:", e)
-        return None
+lock_file = 'periodic_task.lock'
 
 def my_periodic_task():
     print("Running periodic task...")
     print(time.strftime("%Y-%m-%d %H:%M:%S"))
 
 def start_periodic_task():
-    print("Starting periodic task...")
-    def run_periodic():
-        while True:
-            my_periodic_task()
-            time.sleep(5)
+    if not os.path.exists(lock_file):
+        with open(lock_file, 'w') as f:
+            f.write('Task started\n')
+        print("Starting periodic task...")
+        def run_periodic():
+            while True:
+                my_periodic_task()
+                time.sleep(5)
+        periodic_thread = threading.Thread(target=run_periodic)
+        periodic_thread.daemon = True
+        periodic_thread.start()
 
-    # Create a new thread to run the periodic task
-    periodic_thread = threading.Thread(target=run_periodic)
-    # Set the thread as daemon so it will exit when the main thread exits
-    periodic_thread.daemon = True
-    # Start the thread
-    periodic_thread.start()
+def remove_lock_file():
+    try:
+        os.remove(lock_file)
+    except FileNotFoundError:
+        pass
+
+atexit.register(remove_lock_file)
