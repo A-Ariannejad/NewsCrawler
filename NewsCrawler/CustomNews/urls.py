d2d3656c4f76bc19088ea3c822e3c831ab488f25
A_Ariannejad <ariannejad14@gmail.com>
2024-06-05 04:47:44 +0330

    Added: Show by yjc_id api

--- NewsCrawler/CustomNews/urls.py
+++ NewsCrawler/CustomNews/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import CustomNewListView
+from .views import CustomNewListView, CustomNewShowView
 
 urlpatterns = [
+    path('show/<int:yjc_id>/', CustomNewShowView.as_view(), name='show'),
     path('list/', CustomNewListView.as_view(), name='list'),
+
 ]
--- NewsCrawler/CustomNews/views.py
+++ NewsCrawler/CustomNews/views.py
@@ -1,20 +1,25 @@
 from rest_framework import status, generics, viewsets, permissions
 from .serializers import CustomNew, GetCustomNewSerializer
 from CustomUsers.permissions import IsAdminUser
-from rest_framework.generics import ListAPIView
 from rest_framework.pagination import PageNumberPagination
-from .models import CustomNew
 from .serializers import GetCustomNewSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 from django_filters import rest_framework 
 from rest_framework import filters
+from rest_framework.response import Response
+from datetime import datetime
 
 class CustomNewFilter(rest_framework.FilterSet):
     pubDate_ad = rest_framework.DateTimeFromToRangeFilter()
-    pubDate_solar = rest_framework.DateTimeFromToRangeFilter()
     class Meta:
         model = CustomNew
-        fields = '__all__'
+        fields = ['category', 'status', 'pubDate_ad']
+
+class CustomNewCategoriesNumberFilter(rest_framework.FilterSet):
+    pubDate_ad = rest_framework.DateTimeFromToRangeFilter()
+    class Meta:
+        model = CustomNew
+        fields = ['pubDate_ad']
 
 class CustomNewPagination(PageNumberPagination):
     page_size = 5
@@ -26,9 +31,9 @@ class CustomNewPagination(PageNumberPagination):
     page_size_query_param = 'page_size'
     max_page_size = 100
 
-class CustomNewListView(ListAPIView):
+class CustomNewListView(generics.ListAPIView):
     queryset = CustomNew.objects.all()
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [permissions.IsAuthenticated]
     serializer_class = GetCustomNewSerializer
     pagination_class = CustomNewPagination
     filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
@@ -36,6 +41,15 @@ class CustomNewListView(ListAPIView):
     search_fields = ['category', 'title', 'link', 'yjc_id', 'description']
     ordering_fields = ['create_date']
     ordering = ['-create_date']
+
+class CustomNewShowView(generics.RetrieveAPIView):
+    queryset = CustomNew.objects.all()
+    serializer_class = GetCustomNewSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    lookup_field = 'yjc_id'
+
+
+
 
 
 
