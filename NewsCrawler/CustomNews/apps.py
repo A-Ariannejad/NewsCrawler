cfe961ddedabac6ea0b6f9f3676946a97988783e
A_Ariannejad <ariannejad14@gmail.com>
2024-06-05 16:24:24 +0330

    Added: save to database objects for CustomNews from Crawlers

--- NewsCrawler/CustomNews/apps.py
+++ NewsCrawler/CustomNews/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class CustomnewsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'CustomNews'
+
+    def ready(self):
+        from Crawlers.tasks import start_periodic_task  
+        start_periodic_task()
