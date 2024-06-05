f350b8b380732842d940afa6a0539bc4811b3fe7
A_Ariannejad <ariannejad14@gmail.com>
2024-06-05 17:01:57 +0330

    Added: CustomRequestLog model

--- NewsCrawler/CustomRequestLogs/models.py
+++ NewsCrawler/CustomRequestLogs/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class CustomRequestLog(models.Model):
+    timestamp = models.DateTimeField(auto_now_add=True)
+    status = models.BooleanField()
+
+    def __str__(self):
+        return f"{self.timestamp} - {'Success' if self.status else 'Failure'}"
