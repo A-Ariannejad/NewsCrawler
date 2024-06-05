d811173593d4ce26f5390b156253c493db25ad10
A_Ariannejad <ariannejad14@gmail.com>
2024-06-06 01:04:46 +0330

    Fixed: entrypoint file to handle super user as well

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,5 @@
 
 python manage.py makemigrations
 python manage.py migrate --no-input
-python manage.py collectstatic
-
+python manage.py createsuperuser --no-input --username a 
 python manage.py runserver 0.0.0.0:8000
