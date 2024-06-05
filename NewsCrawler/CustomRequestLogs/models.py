b068c2988947c79b3bd1ced4d88c3175d9daffd0
A_Ariannejad <ariannejad14@gmail.com>
2024-06-05 17:05:42 +0330

    Updated: Crawlers model with the exact status

--- NewsCrawler/CustomRequestLogs/models.py
+++ NewsCrawler/CustomRequestLogs/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 class CustomRequestLog(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
-    status = models.BooleanField()
+    status = models.CharField(max_length=50)
+    successful = models.BooleanField(default=False)
 
     def __str__(self):
-        return f"{self.timestamp} - {'Success' if self.status else 'Failure'}"
+        return f"{self.timestamp} - {self.status} - - {'Success' if self.successful else 'Failure'}"
